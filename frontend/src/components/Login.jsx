540199d046862f15c3c10ef86991c22d817ad32e
Chandan Gupta <chandangupta@Chandans-MacBook-Air.local>
2026-07-14 19:35:36 +0530

    fixed cors error

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -29,7 +29,7 @@ const Login = ({ onLogin, sendDataToParent }) => {
     }
     setError('');
     try {
-      const res = await fetch('http://localhost:8000/api/auth/login', {
+      const res = await fetch('https://phoenix-1-l1fw.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
